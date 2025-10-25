3df259337700121f42a9ae8d8c30f4c9346a593e
Isabel <60532846+isabelaranguren@users.noreply.github.com>
2025-10-25 20:42:59 +0000

    wip

--- cache/handle_with_cache.c
+++ cache/handle_with_cache.c
@@ -1,62 +1,73 @@
 #include "gfserver.h"
 #include "cache-student.h"
+#include "shm_channel.h"
 
 #define BUFSIZE (840)
 
-/*
- __.__
-Replace with your implementation
- __.__
-*/
-ssize_t handle_with_cache(gfcontext_t *ctx, const char *path, void* arg){
-	size_t file_len;
-    size_t bytes_transferred;
-	char *data_dir = arg;
-	ssize_t read_len;
-    ssize_t write_len;
-	char buffer[BUFSIZE];
-	int fildes;
-	struct stat statbuf;
+extern shm_data_t *shm_segment;
+extern size_t shm_segsize;  
 
-	strncpy(buffer,data_dir, BUFSIZE);
-	strncat(buffer,path, BUFSIZE);
+ssize_t handle_with_cache(gfcontext_t *ctx, char *path, void* arg) {
+    shm_data_t *shm = shm_segment;  // use global segment
+    size_t file_len;
+    size_t bytes_transferred = 0;
+    ssize_t read_len, write_len;
+    int fd;
+    struct stat statbuf;
 
-	if( 0 > (fildes = open(buffer, O_RDONLY))){
-		if (errno == ENOENT)
-			//If the file just wasn't found, then send FILE_NOT_FOUND code
-			return gfs_sendheader(ctx, GF_FILE_NOT_FOUND, 0);
-		else
-			//Otherwise, it must have been a server error. gfserver library will handle
-			return SERVER_FAILURE;
-	}
+    // Open the requested file
+    fd = open(path, O_RDONLY);
+    if (fd < 0) {
+        if (errno == ENOENT) return gfs_sendheader(ctx, GF_FILE_NOT_FOUND, 0);
+        else return SERVER_FAILURE;
+    }
 
-	//Calculating the file size
-	if (fstat(fildes, &statbuf) < 0) {
-		return SERVER_FAILURE;
-	}
-	file_len = (size_t) statbuf.st_size;
-	///
+    if (fstat(fd, &statbuf) < 0) {
+        close(fd);
+        return SERVER_FAILURE;
+    }
 
-	gfs_sendheader(ctx, GF_OK, file_len);
+    file_len = (size_t) statbuf.st_size;
+    gfs_sendheader(ctx, GF_OK, file_len);
 
-	//Sending the file contents chunk by chunk
+    char buffer[BUFSIZE];
 
-	bytes_transferred = 0;
-	while(bytes_transferred < file_len){
-		read_len = read(fildes, buffer, BUFSIZE);
-		if (read_len <= 0){
-			fprintf(stderr, "handle_with_file read error, %zd, %zu, %zu", read_len, bytes_transferred, file_len );
-			return SERVER_FAILURE;
-		}
-		write_len = gfs_send(ctx, buffer, read_len);
-		if (write_len != read_len){
-			fprintf(stderr, "handle_with_file write error");
-			return SERVER_FAILURE;
-		}
-		bytes_transferred += write_len;
-	}
+    while (bytes_transferred < file_len) {
+        // Read chunk from file
+        read_len = read(fd, buffer, BUFSIZE);
+        if (read_len <= 0) break;
 
-	return bytes_transferred;
+        size_t offset = 0;
+        while (offset < read_len) {
+            // Wait for proxy to read previous data
+            sem_wait(&shm->proxy_read);
 
+            // Determine how much fits in shared memory
+            size_t chunk_size = read_len - offset;
+            if (chunk_size > shm_segsize) chunk_size = shm_segsize;
 
+            // Copy to shared memory
+            memcpy(shm->data, buffer + offset, chunk_size);
+            shm->size = chunk_size;
+
+            // Signal cache written
+            sem_post(&shm->cache_written);
+
+            offset += chunk_size;
+        }
+
+        // Now read back from shared memory into proxy and send to client
+        sem_wait(&shm->cache_written); // Wait until cache wrote
+        write_len = gfs_send(ctx, shm->data, shm->size);
+        if (write_len != shm->size) {
+            close(fd);
+            return SERVER_FAILURE;
+        }
+        sem_post(&shm->proxy_read); // Allow next chunk to be written
+
+        bytes_transferred += read_len;
+    }
+
+    close(fd);
+    return bytes_transferred;
 }
--- cache/shm_channel.h
+++ cache/shm_channel.h
@@ -13,13 +13,25 @@
 #define MAX_CHUNK 8192
 
 typedef struct {
+    pthread_mutex_t lock;
+    int in_use;
+
     sem_t cache_written;   // Signals when cache wrote a chunk
     sem_t proxy_read;      // Signals when proxy read the chunk
-    size_t capacity;  // max usable space
-    size_t size;      // actual data in this chunk
+    
+    int status;
+    size_t chunk_size;      // actual data in this chunk
+    size_t yes;
+    size_t bytes_read;
+
+    size_t capacity;
     char data[MAX_CHUNK];         
 } shm_data_t;
 
-shm_data_t *create_shm_segment(size_t segsize);
-
-void destroy_shm_segment(shm_data_t *shm, size_t segsize);
+// this is for themessage queue command channel 
+typedef struct {
+    char filepath[256];
+    char shm_name[64];
+    int num_segments;
+    pid_t proxy_pid;  // optional, for tracking
+} cache_request_t;
